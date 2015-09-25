b3bf2a08b3e0fb512940ddda36f28bb95d601dd4
Ashley Mason <amason@spoonflower.com>
2015-09-25 16:47:32 -0400

    able to render out individual pieces of information from API.

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,5 +1,5 @@
 class SpotifyController < ApplicationController
   def index
-    render json: Spotify.new().display
+    render json: Spotify.new().get_genre
   end
 end
--- app/models/spotify.rb
+++ app/models/spotify.rb
@@ -4,11 +4,19 @@ class Spotify
     @get_item = get_item
   end
 
-  def display
+  def display_all_info
     @get_item
   end
 
+  def get_genre
+    @get_item["artists"]["items"][0]["genres"][0]
+  end
+
+  def get_uri
+    @get_item["artists"]["items"][0]["uri"]
+  end
+
   def get_item
-    HTTParty.get("https://api.spotify.com/v1/search?q=Dimmu+Borgir&type=artist,album&market=US&limit=1")
+    HTTParty.get("https://api.spotify.com/v1/search?q=Dimmu+Borgir&type=artist&limit=1")
   end
 end
