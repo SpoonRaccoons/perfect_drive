dab8072abc8dc9c9008db3e747f60b6c53480e9c
Josh McKelvey <jmckelvey0@gmail.com>
2015-09-27 12:46:16 -0400

    add notes on where we might want to go

---
+++ app/models/playlist.rb
@@ -0,0 +1,9 @@
+class Playlist
+  # trip_duration = TravelTime.new(params[:origins], params[:desitinations]) Create new TravelTime object.
+  # Create new sample set of songs object using Ashley's code.  Would probably be best as a hash of songs that could be randomly pulled from.  Each hash item would probably be { artist => name, album => name, track => name, track_length => milliseconds }
+  # while trip_duration > song_time_total
+  #   playlist << track_item{artist, album, title, length}        shovel one hash item from Ashleys hash of hashes (or array of hashes) into a playlist.
+  #   song_time_total += track_item{length}
+  # end
+  
+end
